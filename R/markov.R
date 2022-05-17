a0678e2ca138381085fcd38249baa82a7f3f6523
barneyricca <barneyricca@gmail.com>
2022-05-17 13:06:31 -0600

    Added bootMarkov

--- R/markov.R
+++ R/markov.R
@@ -50,6 +50,65 @@ makeMarkov <- function(ts) {   # category vector
 #' @examples
 #' bootMarkov(ts)
 #'
-bootMarkov <- function(ts) {   # category vector
-  return("Not implemented")
+bootMarkov <- function(obs_adj_mat,       # Observed adjacency matrix
+                       obs_node_freq,     # Observed node frequencies
+                       directed = TRUE,   # Directed network?
+                       loops = TRUE,      # Loops in the matrix?
+                       ci = 0.95,         # Confidence interval
+                       R = 1e3) {         # Number of replications
+    # Bootstrap whether the adj_mat entries fall within the
+    #  ci ("black") or not ("blue" for less than, and "green" for
+    #  greater than).
+    # Updates to do:
+    #  Work with non-directed networks
+    #  match a degree distribution
+    #
+    length(obs_node_freq) -> num_nodes
+    if(directed == TRUE) {
+      sum(obs_adj_mat) -> num_edges      # This should be sum(obs_node_freq) - 1, I think
+    }
+    rep(1:num_nodes, obs_node_freq) -> node_pop
+
+    matrix("black",
+           ncol = num_nodes,
+           nrow = num_nodes) -> result_mat
+    array(0,
+          dim = c(num_nodes,
+                  num_nodes,
+                  R)) -> samples_arr
+    array(FALSE,
+          dim = c(num_nodes,
+                  num_nodes,
+                  2)) -> quantile_arr
+
+    for (i in 1:R) {
+      replicate(num_edges,
+                sample(node_pop,
+                       2,
+                       replace = loops)) -> edge_samp
+      for (j in 1:num_edges) {
+        samples_arr[edge_samp[1, j], edge_samp[2, j], i] + 1 ->
+          samples_arr[edge_samp[1, j], edge_samp[2, j], i]
+      }
+    }
+
+    # Now, to get the confidence intervals
+    min_quant = (1 - ci) / 2
+    max_quant = 1 - min_quant
+    for (i in 1:num_nodes) {
+      for (j in 1:num_nodes) {
+        if (obs_adj_mat[i, j] < quantile(samples_arr[i, j, ],
+                                         min_quant)) {
+          "blue" -> result_mat[i, j]
+        } else {
+          if (obs_adj_mat[i, j] > quantile(samples_arr[i, j, ],
+                                           max_quant)) {
+            "green" -> result_mat[i, j]
+          }
+        }
+      }
+    }
+    return(result_mat)
+  }
+
 }
